ea104317a0fdd0b494ae56d7b9115e491da2f18d
mancillajona <jonathan.mancilla@lkmx.io>
2021-10-20 11:27:42 -0700

    - add header and footer in layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,81 @@
 <template>
   <simple-layout>
-    <template #header>
-      <simple-header mode="normal">
-        <img class="header__logo" src="~/assets/img/flare--white.png">
-        <nav>
-          <NuxtLink to="/">Index</NuxtLink>
-          <NuxtLink to="/galactic-flare">Galactic Flare</NuxtLink>
-        </nav>
-      </simple-header>
-    </template>
+    <flui-header
+        mode="full"
+        logo="logo.png"
+        :mainNavigation='[{"title":"Home","to":"/"},{"title":"Products","to":"/"},{"title":"About Us","to":"/"},{"title":"Our Technology","to":"/"},{"title":"Our Services","to":"/"}]'
+        :actions='[{"title":"","to":"/"},{"title":"","to":"/"}]'
+    />
     <Nuxt />
     <template #footer>
       <simple-footer>
-        <p>Flare Project</p>
+        <section class="simple-footer__content">
+          <div class="simple-footer__content__info-skyou">
+            <div class="simple-footer__content__skyou">
+              <img src="../assets/img/logo--white.png" alt="skyou">
+              <div class="simple-footer__content__social-media">
+                <img src="../assets/img/icon-social-facebook.svg" alt="facebook">
+                <img src="../assets/img/icon-social-instagram.svg" alt="instagram">
+              </div>
+            </div>
+            <div class="simple-footer__content__contact">
+              <div class="simple-footer__content__contact__mail">
+                <img src="../assets/img/icon-mail.svg" alt="mail">
+                <p>pr@skyou.com</p>
+              </div>
+              <div class="simple-footer__content__contact__phone">
+                <img src="../assets/img/icon-phone.svg" alt="telephone">
+                <p>(415) 000-0000</p>
+              </div>
+              <div class="simple-footer__content__contact__location">
+                <img src="../assets/img/icon-location-address.svg" alt="location">
+                <p>4096 Piedmont Ave. #130 Oakland CA, 94611 United States</p>
+              </div>                            
+            </div>
+          </div>
+          <div class="simple-footer__content__products">
+            <h4>Products</h4>
+            <div class="simple-footer__content__products__links-container">
+              <nuxt-link to="/">Apparel</nuxt-link>
+              <nuxt-link to="/">Bags</nuxt-link>
+              <nuxt-link to="/">Footwear</nuxt-link>             
+            </div>
+          </div>
+          <div class="simple-footer__content__about-us">
+            <h4>About Us</h4>
+            <div class="simple-footer__content__about-us__links-container">
+              <nuxt-link to="/">Our Story</nuxt-link>
+              <nuxt-link to="/">Contact Us</nuxt-link>         
+            </div>
+          </div>
+          <div class="simple-footer__content__our-technology">
+            <h4>Our Technology</h4>
+            <div class="simple-footer__content__our-technology__links-container">
+              <nuxt-link to="/">3D Design Tool</nuxt-link>
+              <nuxt-link to="/">Inkjet Printing</nuxt-link>
+              <nuxt-link to="/">Cut/Sew</nuxt-link>            
+              <nuxt-link to="/">Drop-Shipping</nuxt-link>              
+            </div>
+          </div>   
+          <div class="simple-footer__content__our-services">
+            <h4>Our Services</h4>
+            <div class="simple-footer__content__our-services__links-container">
+              <nuxt-link to="/">Shopify App</nuxt-link>
+              <nuxt-link to="/">Full Package for Brands</nuxt-link>
+              <nuxt-link to="/">ASI/PPAI</nuxt-link>             
+            </div>
+          </div>                 
+        </section>
       </simple-footer>
+      <section class="simple-footer__legal">
+        <div class="simple-footer__legal__skyou-legend">
+          <p>© SKYOU 2021. All rights reserved.</p>
+        </div>
+        <div class="simple-footer__legal__skyou-links">
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+        </div>
+      </section>
     </template>
   </simple-layout>
 </template>
@@ -26,10 +88,169 @@
 </script>
 
 <style lang="scss">
-.header {
-  &__logo {
-    height: 60px;
-    padding: var(--f-gutter-s) var(--f-gutter);
+  @import '@lkmx/flare/src/functions/_respond-to.scss';
+:root {
+
+}
+
+#__nuxt {
+  .simple-footer {
+    background-color: var(--color-neutral-01);
+    padding: var(--f-gutter-xl) 0;
+    @include respond-to('<=m') {
+      padding: 0;
+    }
+    &__content {
+      display: grid;
+      grid-template-columns: repeat(5, 1fr); 
+      /* grid-auto-flow: column;  */
+      @include respond-to('<=m') {
+        grid-template-columns: 1fr;
+        grid-template-rows: repeat(5, auto);
+        /* grid-auto-flow: row; */
+      }
+      &__info-skyou {
+        display: grid;
+        gap: var(--f-gutter-xl);
+        padding: var(--f-gutter);
+        @include respond-to('<=m') {
+          padding: var(--f-gutter-l) 0;
+        }
+      }
+      &__skyou {
+        display: grid;
+        gap: var(--f-gutter);
+        @include respond-to('<=m') {
+          justify-content: center;
+        }
+      }
+      &__social-media {
+        display: grid;
+        grid-auto-flow: column;
+        justify-content: start;
+        gap: 10px;
+        @include respond-to('<=m') {
+          justify-content: center;
+        }
+      }
+      &__contact {
+        display: grid;
+        gap: var(--f-gutter);
+        &__mail, &__phone,&__location {
+          display: grid;
+          grid-auto-flow: column;
+          justify-content: start;
+          gap: 10px;
+          p {
+            margin-bottom: 0;
+          }
+          @include respond-to('<=m') {
+            grid-auto-flow: row;
+            justify-content: center;
+            gap: 10px;
+            img {
+              display: block;
+              margin: auto;
+            }
+            p {
+              text-align: center;
+            }
+          }
+        }  
+        &__location {
+          p {
+            @include respond-to(">=l") {
+              /* width: 42%; */
+            }
+          }
+        }              
+        p {
+          color: var(--color-neutral-10);
+        }
+      }
+      &__products, &__about-us, &__our-technology, &__our-services {
+        display: grid;
+        grid-template-rows: auto 1fr;
+        gap: var(--f-gutter);
+        padding: var(--f-gutter);
+        @include respond-to('<=m') {
+          padding: var(--f-gutter-l) 0;
+          justify-content: center;
+          text-align: center;
+        }     
+        h4 {
+          color: var(--color-neutral-10);
+          margin: 0;
+        }
+        &__links-container {
+          display: grid;
+          grid-auto-flow: row;
+          gap: 10px; 
+          height: max-content;
+          a {
+            text-decoration: none;
+            color: var(--color-neutral-10);
+          }
+        }
+      }
+    }
+    &__legal {
+      display: grid;
+      grid-template-columns: 927px auto;
+      padding: var(--f-gutter) 0;
+      background-color: var(--color-neutral-02);
+      justify-content: center;
+      @include respond-to('<=l') {
+        grid-template-columns: 1fr auto;
+
+        @include respond-to('<=m') {
+          grid-template-columns: 1fr;
+          padding: 0;
+        }
+      }
+      p {
+        margin: 0;
+        color: var(--color-neutral-10);
+      }
+      &__skyou-legend {
+        display: grid;
+        align-content: center;
+        padding: var(--f-gutter); 
+        @include respond-to('<=m') {
+          justify-content: center;
+          padding: var(--f-gutter-l) var(--f-gutter); 
+        }
+      }
+      &__skyou-links {
+        display: grid;
+        grid-auto-flow: column;
+        align-content: center;
+        padding: var(--f-gutter);
+        gap: var(--f-gutter-l);
+        @include respond-to('<=m') {
+          justify-content: center;
+          grid-auto-flow: row;
+          padding: var(--f-gutter-l) var(--f-gutter); 
+          gap: var(--f-gutter);
+          p {
+            text-align: center;
+          }
+        }        
+      }
+    }
+    .--flare {
+      .content {
+        @include respond-to('<=m') {
+          width: 100%;
+        }
+        .box {
+          padding: 0;
+          @include respond-to('<=m') {
+            padding: 0 var(--f-gutter);
+          }
+        }
+      }
+    }
   }
 }
 </style>
