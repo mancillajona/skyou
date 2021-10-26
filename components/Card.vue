ed8dbbff1eb900697ddde0c1a0ed7bab0f6c4525
mancillajona <jonathan.mancilla@lkmx.io>
2021-10-26 15:13:24 -0700

    - add card.vue in componens

---
+++ components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card__content">
+    <h3>{{title}}</h3>
+    <p class="card__content--description">{{subtitle}}</p>
+    <slot name="image"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    subtitle: {
+      tytpe: String,
+      required: false,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .card {
+    &__content {
+      display: grid;
+      gap: var(--f-gutter);
+      h3, p {
+        margin-bottom: 0;
+      }
+      h3 {
+        color: var(--color-neutral-03);
+      }
+      &--description {
+        height: 87px;
+      }
+    }
+  }
+</style>
