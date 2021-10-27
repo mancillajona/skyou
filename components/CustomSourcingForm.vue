4cfa1d3abbc1184fa8e577af81b25c56d669c4d9
mancillajona <jonathan.mancilla@lkmx.io>
2021-10-27 10:39:32 -0700

    - add CustomSourcingForm.vue in component

---
+++ components/CustomSourcingForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe src="https://forms.monday.com/forms/embed/f5b88f3115f3af9ec6f0c45f4eb4a302?r=use1" 
+  width="650" height="3150" style="border: 0;">
+  </iframe>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "@lkmx/flare/src/functions/_respond-to.scss";
+iframe {
+  @include respond-to("<=m") {
+    width: 100vw;
+  }
+}
+</style>
